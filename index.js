0afc039fb9aae12a240b0c0467d343c113fdf912
noelensith <mbnoelen@gmail.com>
2022-04-04 18:21:58 -0700

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(record) {
+    function isWinning(year) {
+        if (year.result === 'W') {
+            return true
+        }
+    }
+
+    const winningYearObject = record.find(isWinning)
+
+    if (winningYearObject) {
+        return winningYearObject.year
+    }
+}
